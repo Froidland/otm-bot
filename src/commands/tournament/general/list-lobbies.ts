09633179ac19cea2478908039ca81e56f089f373
Francisco Hauva <phauva@gmail.com>
2023-07-12 01:47:52 -0400

    remove remaining outdated command.

--- src/commands/tournament/general/list-lobbies.ts
+++
@@ -1,9 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-import { Command } from "@/interfaces/command";
-
-export const template: Command = {
-	data: new SlashCommandBuilder()
-		.setName("template")
-		.setDescription("Template"),
-	execute: async (interaction: ChatInputCommandInteraction) => {},
-};
