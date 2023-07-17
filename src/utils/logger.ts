18b6a07ac2e1b70fbe63099e3416cee21cd1d8c7
Francisco Hauva <phauva@gmail.com>
2023-07-16 21:52:53 -0400

    Separate transport array into a const and add axiom logging.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,69 @@
 import DailyRotateFile from "winston-daily-rotate-file";
 import * as winston from "winston";
-const { combine, timestamp, printf, colorize } = winston.format;
+import { WinstonTransport as AxiomTransport } from "@axiomhq/axiom-node";
+import "dotenv/config";
+const { combine, timestamp, printf, colorize, errors, json } = winston.format;
 const logDatePattern = process.env.LOG_DATE_PATTERN ?? "DD-MM-YYYY";
 
 // TODO: Don't really know if the logger should be in the utils folder.
-// TODO: I'm thinking about using a different logging library, maybe Pino. Need to do some research.
+const transports: winston.transport[] = [
+	new DailyRotateFile({
+		filename: "panchobot-error-%DATE%",
+		extension: ".log",
+		dirname: "logs",
+		datePattern: logDatePattern,
+		zippedArchive: true,
+		maxSize: "20m",
+		maxFiles: "14d",
+		level: "error",
+		format: combine(
+			timestamp(),
+			printf(({ timestamp, level, message }) => {
+				return `${timestamp} [${level}] ${message}`;
+			})
+		),
+	}),
+	new DailyRotateFile({
+		filename: "panchobot-main-%DATE%",
+		extension: ".log",
+		dirname: "logs",
+		datePattern: logDatePattern,
+		zippedArchive: true,
+		maxSize: "20m",
+		maxFiles: "14d",
+		format: combine(
+			timestamp(),
+			printf(({ timestamp, level, message }) => {
+				return `${timestamp} [${level}] ${message}`;
+			})
+		),
+	}),
+	new winston.transports.Console({
+		format: winston.format.combine(
+			timestamp(),
+			colorize(),
+			printf(({ timestamp, level, message }) => {
+				return `${timestamp} [${level}] ${message}`;
+			})
+		),
+	}),
+];
+
+if (
+	process.env.AXIOM_DATASET &&
+	process.env.AXIOM_TOKEN &&
+	process.env.AXIOM_ORG_ID
+) {
+	transports.push(
+		new AxiomTransport({
+			dataset: process.env.AXIOM_DATASET,
+			token: process.env.AXIOM_TOKEN,
+			orgId: process.env.AXIOM_ORG_ID,
+			format: combine(errors({ stack: true }), json()),
+		})
+	);
+}
+
 export const logger = winston.createLogger({
 	level: process.env.NODE_ENV === "development" ? "debug" : "info",
 	format: combine(
@@ -13,48 +72,7 @@ export const logger = winston.createLogger({
 			return `${timestamp} [${level}] ${message}`;
 		})
 	),
-	transports: [
-		new DailyRotateFile({
-			filename: "panchobot-error-%DATE%",
-			extension: ".log",
-			dirname: "logs",
-			datePattern: logDatePattern,
-			zippedArchive: true,
-			maxSize: "20m",
-			maxFiles: "14d",
-			level: "error",
-			format: combine(
-				timestamp(),
-				printf(({ timestamp, level, message }) => {
-					return `${timestamp} [${level}] ${message}`;
-				})
-			),
-		}),
-		new DailyRotateFile({
-			filename: "panchobot-main-%DATE%",
-			extension: ".log",
-			dirname: "logs",
-			datePattern: logDatePattern,
-			zippedArchive: true,
-			maxSize: "20m",
-			maxFiles: "14d",
-			format: combine(
-				timestamp(),
-				printf(({ timestamp, level, message }) => {
-					return `${timestamp} [${level}] ${message}`;
-				})
-			),
-		}),
-		new winston.transports.Console({
-			format: winston.format.combine(
-				timestamp(),
-				colorize(),
-				printf(({ timestamp, level, message }) => {
-					return `${timestamp} [${level}] ${message}`;
-				})
-			),
-		}),
-	],
+	transports,
 });
 
 export default logger;
