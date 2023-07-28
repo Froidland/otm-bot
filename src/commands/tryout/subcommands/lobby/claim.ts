c220932a12f9e07e7b24359e23ceea681c28d1ac
Francisco Hauva <phauva@gmail.com>
2023-07-27 21:42:07 -0400

    Add success message.

--- src/commands/tryout/subcommands/lobby/claim.ts
+++ src/commands/tryout/subcommands/lobby/claim.ts
@@ -36,7 +36,7 @@ export const claim: SubCommand = {
 
 		const lobbyId = interaction.options.getString("lobby-id", true);
 
-		// Get the tryout for the current staff channel and include any lobbies that match the lobby ID.
+		//? Get the tryout for the current staff channel and include any lobbies that match the lobby ID.
 		const tryout = await db.tryout.findFirst({
 			where: {
 				staffChannelId: interaction.channelId,
@@ -112,6 +112,17 @@ export const claim: SubCommand = {
 				data: {
 					refereeId: user.id,
 				},
+			});
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Green")
+						.setTitle("Success")
+						.setDescription(
+							`You have successfully claimed the lobby ${lobby.customId}.`
+						),
+				],
 			});
 		} catch (error) {
 			await interaction.editReply({
