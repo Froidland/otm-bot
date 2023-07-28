568691badcc4eb0ec1c3861ff204318fba55cd1b
Francisco Hauva <phauva@gmail.com>
2023-07-28 03:08:59 -0400

    Add tryout embed send command.

--- src/commands/tryout/subcommands/embed/send.ts
+++ src/commands/tryout/subcommands/embed/send.ts
@@ -1,7 +1,13 @@
+import db from "@/db";
+import { NoAccountEmbed } from "@/embeds";
+import { tryoutRegistration } from "@/interactive-messages";
 import { SubCommand } from "@/interfaces/subCommand";
+import { logger } from "@/utils";
 import {
 	ChannelType,
 	ChatInputCommandInteraction,
+	EmbedBuilder,
+	GuildTextBasedChannel,
 	SlashCommandSubcommandBuilder,
 } from "discord.js";
 
@@ -19,6 +25,109 @@ export const send: SubCommand = {
 				.addChannelTypes(ChannelType.GuildText)
 		),
 	execute: async (interaction: ChatInputCommandInteraction) => {
-		
+		await interaction.deferReply();
+		let wasPreviousEmbedDeleted = false;
+
+		const user = await db.user.findFirst({
+			where: {
+				discordId: interaction.user.id,
+			},
+		});
+
+		if (!user) {
+			await interaction.editReply({
+				embeds: [NoAccountEmbed],
+			});
+
+			return;
+		}
+
+		const tryout = await db.tryout.findFirst({
+			where: {
+				staffChannelId: interaction.channelId,
+			},
+		});
+
+		if (!tryout) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription("This channel is not a tryout staff channel."),
+				],
+			});
+
+			return;
+		}
+
+		const channel = interaction.options.getChannel(
+			"channel",
+			true
+		) as GuildTextBasedChannel;
+
+		const previousChannel = await interaction.guild?.channels.fetch(
+			tryout.embedChannelId
+		);
+
+		if (previousChannel) {
+			try {
+				const messages = await (
+					previousChannel as GuildTextBasedChannel
+				).messages.fetch({
+					around: tryout.embedMessageId,
+				});
+
+				const previousMessage = messages.get(tryout.embedMessageId);
+
+				if (previousMessage) {
+					await previousMessage.delete();
+					wasPreviousEmbedDeleted = true;
+				}
+			} catch (error) {
+				logger.error(error);
+			}
+		}
+
+		try {
+			const newMessage = await channel.send(tryoutRegistration(tryout.name));
+
+			await db.tryout.update({
+				where: {
+					id: tryout.id,
+				},
+				data: {
+					embedChannelId: channel.id,
+					embedMessageId: newMessage.id,
+				},
+			});
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor(wasPreviousEmbedDeleted ? "Green" : "Yellow")
+						.setTitle("Success")
+						.setDescription(
+							`The tryout embed has been sent successfully to ${channel}` +
+								(wasPreviousEmbedDeleted
+									? "."
+									: " but there was an error while trying to delete the previous one. Please delete it manually.")
+						),
+				],
+			});
+		} catch (error) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription(
+							"An error occurred while sending the tryout embed."
+						),
+				],
+			});
+
+			return;
+		}
 	},
 };
