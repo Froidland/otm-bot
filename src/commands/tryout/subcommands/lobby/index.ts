1b72197c817881c3fa13aeb5fd6c33b3e0f1ce1e
Francisco Hauva <phauva@gmail.com>
2023-08-31 20:57:45 -0400

    feat: add tryout lobby unclaim command

--- src/commands/tryout/subcommands/lobby/index.ts
+++ src/commands/tryout/subcommands/lobby/index.ts
@@ -5,6 +5,7 @@ import { claim } from "./claim";
 import { join } from "./join";
 import { list } from "./list";
 import { leave } from "./leave";
+import { unclaim } from "./unclaim";
 
 const lobbyGroup: SubCommandGroup = {
 	data: new SlashCommandSubcommandGroupBuilder()
@@ -12,10 +13,11 @@ const lobbyGroup: SubCommandGroup = {
 		.setDescription("Commands for managing tryout lobbies.")
 		.addSubcommand(create.data)
 		.addSubcommand(claim.data)
+		.addSubcommand(unclaim.data)
 		.addSubcommand(join.data)
 		.addSubcommand(leave.data)
 		.addSubcommand(list.data),
-	subCommands: [create, claim, join, leave, list],
+	subCommands: [create, claim, unclaim, join, leave, list],
 };
 
 export default lobbyGroup;
