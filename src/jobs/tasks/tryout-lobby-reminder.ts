baf96b5e898f4c77f06b38d5d70206000178d469
Francisco Hauva <phauva@gmail.com>
2023-09-01 02:31:05 -0400

    feat: add initial implementation of tryout lobby reminders

---
+++ src/jobs/tasks/tryout-lobby-reminder.ts
@@ -0,0 +1,154 @@
+import db from "@/db";
+import DiscordBot from "@/discordBot";
+import { logger } from "@/utils";
+import { DateTime, Duration } from "luxon";
+import { AsyncTask } from "toad-scheduler";
+
+// TODO: Needs a revisit cause this looks too convoluted.
+const tryoutLobbyReminderTask = new AsyncTask(
+	"tryout-lobby-reminder",
+	async () => {
+		logger.info("Checking for pending reminders...");
+		const currentDate = DateTime.now();
+
+		const dateThreshold = currentDate.plus(
+			Duration.fromObject({
+				minutes: 15,
+			}),
+		);
+
+		db.tryoutLobby
+			.findMany({
+				where: {
+					schedule: {
+						gt: currentDate.toJSDate(),
+						lt: dateThreshold.toJSDate(),
+					},
+					isReminderSent: false,
+				},
+				include: {
+					players: {
+						select: {
+							player: {
+								select: {
+									discord_id: true,
+								},
+							},
+						},
+					},
+					stage: {
+						include: {
+							tryout: {
+								select: {
+									staff_channel_id: true,
+									referee_role_id: true,
+									player_channel_id: true,
+								},
+							},
+						},
+					},
+					referee: true,
+					_count: {
+						select: {
+							players: true,
+						},
+					},
+				},
+			})
+			.then(async (lobbies) => {
+				if (lobbies.length === 0) {
+					logger.info("No pending reminders.");
+					return;
+				}
+
+				logger.info(`Seding ${lobbies.length} reminders...`);
+				const fulfilledReminderLobbies = [];
+
+				for (const lobby of lobbies) {
+					const messagePromises = [];
+					if (lobby._count.players === 0) {
+						logger.info(
+							`Skipping lobby ${lobby.id} due to not having any players.`,
+						);
+						continue;
+					}
+
+					const playerMentions = lobby.players.map((player) => {
+						return `<@${player.player.discord_id}>`;
+					});
+
+					const playerChannel = await DiscordBot.channels.fetch(
+						lobby.stage.tryout.player_channel_id,
+					);
+
+					const staffChannel = await DiscordBot.channels.fetch(
+						lobby.stage.tryout.staff_channel_id,
+					);
+
+					if (playerChannel && playerChannel.isTextBased()) {
+						messagePromises.push(
+							playerChannel.send({
+								content: `Lobby \`${lobby.custom_id}\` starts in <t:${
+									lobby.schedule.getTime() / 1000
+								}:R>\n${playerMentions.join(" ")}`,
+							}),
+						);
+					}
+
+					if (staffChannel && staffChannel.isTextBased()) {
+						if (lobby.referee) {
+							messagePromises.push(
+								staffChannel.send({
+									content: `Lobby \`${lobby.custom_id}\` starts in <t:${
+										lobby.schedule.getTime() / 1000
+									}:R>, make sure you are able to ref it or unclaim it so that another referee can take it`,
+								}),
+							);
+						} else {
+							messagePromises.push(
+								staffChannel.send({
+									content: `Lobby \`${lobby.custom_id}\` starts in <t:${
+										lobby.schedule.getTime() / 1000
+									}:R> and has no referee. <@${
+										lobby.stage.tryout.referee_role_id
+									}>`,
+								}),
+							);
+						}
+					}
+
+					//? If any of the messages got sent, we mark the reminder as sent.
+					const messageSent = (await Promise.allSettled(messagePromises)).some(
+						(result) => {
+							result.status === "fulfilled";
+						},
+					);
+
+					if (messageSent) {
+						fulfilledReminderLobbies.push(lobby.id);
+					}
+				}
+
+				return fulfilledReminderLobbies;
+			})
+			.then(async (fulfilledReminders) => {
+				if (!fulfilledReminders) {
+					return;
+				}
+
+				await db.tryoutLobby.updateMany({
+					where: {
+						id: {
+							in: fulfilledReminders,
+						},
+					},
+					data: {
+						isReminderSent: true,
+					},
+				});
+			});
+	},
+	(error: Error) => logger.error(error),
+);
+
+export default tryoutLobbyReminderTask;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { auth } from "osu-api-extended";
 import { logger } from "./utils/";
 import DiscordBot from "./discordBot";
 import db from "./db";
+import { scheduler } from "./jobs";
+import { SimpleIntervalJob } from "toad-scheduler";
+import tryoutLobbyReminderTask from "./jobs/tasks/tryout-lobby-reminder";
 
 // TODO: Implement runtime checks for the environment variables. (Maybe use zod for this)
 //? Take a look at https://github.com/lostfictions/znv
@@ -20,6 +23,21 @@ async function bootstrap() {
 		await DiscordBot.login(process.env.BOT_TOKEN);
 
 		await db.$connect();
+
+		// TODO: Use something like BullMQ to queue all the reminders and limit the concurrency.
+		// TODO: Consider registering the jobs once the discord client is ready.
+		scheduler.addSimpleIntervalJob(
+			new SimpleIntervalJob(
+				{
+					minutes: 1,
+				},
+				tryoutLobbyReminderTask,
+				{
+					preventOverrun: true,
+				},
+			),
+		);
+		logger.info("Registered background jobs.");
 	} catch (error) {
 		logger.error(
 			`There was an error while trying to start the bot. Reason: ${error}`,
