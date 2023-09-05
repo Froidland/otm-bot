3ed20b51a254486c99370e3fd926eaddfcc73889
Francisco Hauva <phauva@gmail.com>
2023-09-05 00:39:58 -0300

    feat: add tryout stage pick-remove command for removing maps from a stage's mappool
    closes #17

--- src/commands/tryout/subcommands/stage/index.ts
+++ src/commands/tryout/subcommands/stage/index.ts
@@ -2,14 +2,16 @@ import { SubCommandGroup } from "@/interfaces";
 import { SlashCommandSubcommandGroupBuilder } from "discord.js";
 import { create } from "./create";
 import { pickSet } from "./pick-set";
+import { pickRemove } from "./pick-remove";
 
 const stageGroup: SubCommandGroup = {
 	data: new SlashCommandSubcommandGroupBuilder()
 		.setName("stage")
 		.setDescription("Commands for managing tryout stages.")
 		.addSubcommand(create.data)
-		.addSubcommand(pickSet.data),
-	subCommands: [create, pickSet],
+		.addSubcommand(pickSet.data)
+		.addSubcommand(pickRemove.data),
+	subCommands: [create, pickSet, pickRemove],
 };
 
 export default stageGroup;
