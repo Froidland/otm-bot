a9eeb4e79664d032c7bc4a59b9e303cf61ff859e
Francisco Hauva <phauva@gmail.com>
2023-10-05 04:34:14 -0300

    feat: ported background jobs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import "@sapphire/plugin-logger";
 import { GatewayIntentBits } from "discord.js";
 import db from "./db";
 import { auth } from "osu-api-extended";
+import { scheduler } from "./background-jobs";
+import { SimpleIntervalJob } from "toad-scheduler";
+import tryoutLobbyReminderTask from "./background-jobs/tasks/tryout-lobby-reminder";
 
 async function bootstrap() {
 	const client = new SapphireClient({
@@ -23,6 +26,20 @@ async function bootstrap() {
 		await client.login(process.env.BOT_TOKEN);
 
 		await db.$connect();
+
+		scheduler.addSimpleIntervalJob(
+			new SimpleIntervalJob(
+				{
+					minutes: 1,
+				},
+				tryoutLobbyReminderTask,
+				{
+					preventOverrun: true,
+				},
+			),
+		);
+
+		container.logger.info("Registered background jobs.");
 	} catch (error) {
 		container.logger.error(
 			`There was an error while trying to start the bot. Reason: ${error}`,
