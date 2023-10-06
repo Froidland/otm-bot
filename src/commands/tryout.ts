0b73a9f4a329b389081797ca9d36d1b2b8f4fb61
Francisco Hauva <phauva@gmail.com>
2023-10-06 04:45:38 -0300

    feat: port embed send command

--- src/commands/tryout.ts
+++ src/commands/tryout.ts
@@ -55,6 +55,16 @@ import { v2 } from "osu-api-extended";
 				},
 			],
 		},
+		{
+			name: "embed",
+			type: "group",
+			entries: [
+				{
+					name: "send",
+					chatInputRun: "chatInputEmbedSend",
+				},
+			],
+		},
 	],
 })
 export class TryoutCommand extends Subcommand {
@@ -281,6 +291,27 @@ export class TryoutCommand extends Subcommand {
 										.setName("stage-id")
 										.setDescription("The custom ID of the stage to publish.")
 										.setRequired(true),
+								),
+						),
+				)
+				.addSubcommandGroup((builder: SlashCommandSubcommandGroupBuilder) =>
+					builder
+						.setName("embed")
+						.setDescription("Commands for managing the tryout embed.")
+						.addSubcommand((builder: SlashCommandSubcommandBuilder) =>
+							builder
+								.setName("send")
+								.setDescription(
+									"Resends the tryout embed to the specified channel, deleting the old one.",
+								)
+								.addChannelOption((option) =>
+									option
+										.setName("channel")
+										.setDescription(
+											"The channel where the tryout embed will be sent.",
+										)
+										.setRequired(true)
+										.addChannelTypes(ChannelType.GuildText),
 								),
 						),
 				),
@@ -1384,6 +1415,121 @@ export class TryoutCommand extends Subcommand {
 						),
 				],
 			});
+		}
+	}
+
+	// TODO: This shouldn't be able to be executed by anyone.
+	public async chatInputEmbedSend(
+		interaction: Subcommand.ChatInputCommandInteraction,
+	) {
+		await interaction.deferReply();
+		let wasPreviousEmbedDeleted = false;
+
+		const user = await db.user.findFirst({
+			where: {
+				discord_id: interaction.user.id,
+			},
+		});
+
+		if (!user) {
+			await interaction.editReply({
+				embeds: [NoAccountEmbed],
+			});
+
+			return;
+		}
+
+		const tryout = await db.tryout.findFirst({
+			where: {
+				staff_channel_id: interaction.channelId,
+			},
+		});
+
+		if (!tryout) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription("This channel is not a tryout staff channel."),
+				],
+			});
+
+			return;
+		}
+
+		const channel = interaction.options.getChannel(
+			"channel",
+			true,
+		) as GuildTextBasedChannel;
+
+		if (tryout.embed_channel_id && tryout.embed_message_id) {
+			const previousChannel = await interaction.guild?.channels.fetch(
+				tryout.embed_channel_id,
+			);
+
+			if (previousChannel) {
+				try {
+					const messages = await (
+						previousChannel as GuildTextBasedChannel
+					).messages.fetch({
+						around: tryout.embed_message_id,
+						limit: 1,
+					});
+
+					const previousMessage = messages.get(tryout.embed_message_id);
+
+					if (previousMessage) {
+						await previousMessage.delete();
+						wasPreviousEmbedDeleted = true;
+					}
+				} catch (error) {
+					this.container.logger.error(error);
+				}
+			}
+		} else {
+			wasPreviousEmbedDeleted = true;
+		}
+
+		try {
+			const newMessage = await channel.send(tryoutRegistration(tryout.name));
+
+			await db.tryout.update({
+				where: {
+					id: tryout.id,
+				},
+				data: {
+					embed_channel_id: channel.id,
+					embed_message_id: newMessage.id,
+				},
+			});
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor(wasPreviousEmbedDeleted ? "Green" : "Yellow")
+						.setTitle("Success")
+						.setDescription(
+							`The tryout embed has been sent successfully to ${channel}` +
+								(wasPreviousEmbedDeleted
+									? "."
+									: " but there was an error while trying to delete the previous one. Please delete it manually."),
+						),
+				],
+			});
+		} catch (error) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription(
+							"An error occurred while sending the tryout embed.",
+						),
+				],
+			});
+
+			return;
 		}
 	}
 }
