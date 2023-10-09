a9161f68741d4b0a6f7923bc96f78c152b752441
Francisco Hauva <phauva@gmail.com>
2023-10-08 23:32:03 -0300

    feat: add tryout edit start-date command

--- src/commands/tryout.ts
+++ src/commands/tryout.ts
@@ -1933,7 +1933,124 @@ export class TryoutCommand extends Subcommand {
 	public async chatInputEditStartDate(
 		interaction: Subcommand.ChatInputCommandInteraction,
 	) {
-		await interaction.deferReply();
+		await interaction.deferReply({
+			ephemeral: true,
+		});
+
+		const date = DateTime.fromFormat(
+			interaction.options.getString("value", true),
+			"yyyy-MM-dd HH:mm",
+			{
+				zone: "utc",
+			},
+		);
+
+		const user = await db.user.findFirst({
+			where: {
+				discord_id: interaction.user.id,
+			},
+		});
+
+		if (!user) {
+			await interaction.editReply({
+				embeds: [NoAccountEmbed],
+			});
+
+			return;
+		}
+
+		const tryout = await db.tryout.findFirst({
+			where: {
+				OR: [
+					{
+						staff_channel_id: interaction.channelId,
+					},
+					{
+						player_channel_id: interaction.channelId,
+					},
+				],
+			},
+		});
+
+		if (!tryout) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Invalid channel!")
+						.setDescription(
+							"This command can only be used in a tryout channel.",
+						),
+				],
+			});
+
+			return;
+		}
+
+		if (!isUserTryoutAdmin(interaction, tryout)) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Invalid permissions!")
+						.setDescription("You don't have permission to do this."),
+				],
+			});
+
+			return;
+		}
+
+		if (!date.isValid) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Invalid date!")
+						.setDescription(
+							"The date you specified is invalid. Please use the format `yyyy-MM-dd HH:mm`.",
+						),
+				],
+			});
+
+			return;
+		}
+
+		try {
+			await db.tryout.update({
+				where: {
+					id: tryout.id,
+				},
+				data: {
+					start_date: date.toJSDate(),
+				},
+			});
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Green")
+						.setTitle("Success")
+						.setDescription(
+							`The tryout start date has been updated from \`${DateTime.fromJSDate(
+								tryout.start_date,
+							).toRFC2822()}\` to \`${date.toRFC2822()}\`.`,
+						),
+				],
+			});
+		} catch (error) {
+			this.container.logger.error(error);
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription(
+							"An error occurred while updating the tryout start date. Please try again later.",
+						),
+				],
+			});
+		}
 	}
 
 	public async chatInputEditEndDate(
