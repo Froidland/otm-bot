44076db356414878805a1744a9688607d4585289
Francisco Hauva <phauva@gmail.com>
2023-10-08 22:53:38 -0300

    feat: add tryout edit name command

--- src/commands/tryout.ts
+++ src/commands/tryout.ts
@@ -1732,7 +1732,101 @@ export class TryoutCommand extends Subcommand {
 	public async chatInputEditName(
 		interaction: Subcommand.ChatInputCommandInteraction,
 	) {
-		await interaction.deferReply();
+		await interaction.deferReply({
+			ephemeral: true,
+		});
+
+		const name = interaction.options.getString("value", true);
+
+		const user = await db.user.findFirst({
+			where: {
+				discord_id: interaction.user.id,
+			},
+		});
+
+		if (!user) {
+			await interaction.editReply({
+				embeds: [NoAccountEmbed],
+			});
+
+			return;
+		}
+
+		const tryout = await db.tryout.findFirst({
+			where: {
+				OR: [
+					{
+						staff_channel_id: interaction.channelId,
+					},
+					{
+						player_channel_id: interaction.channelId,
+					},
+				],
+			},
+		});
+
+		if (!tryout) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Invalid channel!")
+						.setDescription(
+							"This command can only be used in a tryout channel.",
+						),
+				],
+			});
+
+			return;
+		}
+
+		if (!isUserTryoutAdmin(interaction, tryout)) {
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Invalid permissions!")
+						.setDescription("You don't have permission to do this."),
+				],
+			});
+
+			return;
+		}
+
+		try {
+			await db.tryout.update({
+				where: {
+					id: tryout.id,
+				},
+				data: {
+					name,
+				},
+			});
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Green")
+						.setTitle("Success")
+						.setDescription(
+							`The tryout name has been updated from \`${tryout.name}\` to \`${name}\`.`,
+						),
+				],
+			});
+		} catch (error) {
+			this.container.logger.error(error);
+
+			await interaction.editReply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor("Red")
+						.setTitle("Error")
+						.setDescription(
+							"An error occurred while updating the tryout name. Please try again later.",
+						),
+				],
+			});
+		}
 	}
 
 	public async chatInputEditAcronym(
